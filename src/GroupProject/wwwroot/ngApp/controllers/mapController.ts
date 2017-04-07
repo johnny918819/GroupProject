c5b9d32ea5f4f6db00e3debf7dad4de2c762b04a
Cort Shields <quartalon@yahoo.com>
2017-04-07 07:53:14 -0700

    Database updates and minor changes

--- src/GroupProject/wwwroot/ngApp/controllers/mapController.ts
+++ src/GroupProject/wwwroot/ngApp/controllers/mapController.ts
@@ -2,35 +2,162 @@
 //-----------------------------------------------Gino: This is the map controller
 
     export class MapController {
-        public message = 'This is Live from the Map View';
-        public center = { latitude: 47.671853, longitude: -122.121328 };
-        public zoom = 4;
+        public message = 'LTB Members Near You!';
+        public center = { latitude: 33.557789, longitude: -111.889232 };
+        public zoom = 11;
         public markers = [
             {
                 id: 0,
                 options: {
-                    title: 'Seattle Coder Camps',
+                    title: 'maryjo',
                 },
-                icon: '/coder-camps-logo.png',
-                latitude: 47.671853,
-                longitude: -122.121328
+                latitude: 33.561991,
+                longitude: -111.903803
             },
             {
                 id: 1,
                 options: {
-                    title: 'Houston Coder Camps',
+                    title: 'talwik',
                 },
-                latitude: 29.552561,
-                longitude: -95.395219
+                latitude: 33.553005,
+                longitude: -111.903288
+            },
+            {
+                id: 2,
+                options: {
+                    title: 'luckyday',
+                },
+                latitude: 33.569196,
+                longitude: -111.871595
             },
             {
                 id: 3,
                 options: {
-                    title: 'San Francisco Coder Camps',
+                    title: 'streetjazz',
                 },
-                latitude: 37.809499,
-                longitude: -122.253665
-            }
+                latitude: 33.647912,
+                longitude: -111.878785
+            },
+            {
+                id: 4,
+                options: {
+                    title: 'godfather',
+                },
+                latitude: 33.645977,
+                longitude: -111.862642
+            },
+            {
+                id: 5,
+                options: {
+                    title: 'cheezzzee',
+                },
+                latitude: 33.612624,
+                longitude: -111.904728
+            },
+            {
+                id: 6,
+                options: {
+                    title: 'itsadele',
+                },
+                latitude: 33.532732,
+                longitude: -111.937590
+            },
+            {
+                id: 7,
+                options: {
+                    title: 'hammer',
+                },
+                latitude: 33.528592,
+                longitude: -111.932728
+            },
+            {
+                id: 8,
+                options: {
+                    title: 'hammer',
+                },
+                latitude: 33.528592,
+                longitude: -111.932728
+            },
+            {
+                id: 9,
+                options: {
+                    title: 'mrjoshua',
+                },
+                latitude: 33.504606,
+                longitude: -111.980749
+            },
+            {
+                id: 10,
+                options: {
+                    title: 'katyp',
+                },
+                latitude: 33.470170,
+                longitude: -111.893419
+            },
+            {
+                id: 11,
+                options: {
+                    title: 'itsadele',
+                },
+                latitude: 33.532732,
+                longitude: -111.937590
+            },
+            {
+                id: 12,
+                options: {
+                    title: 'mrkimk',
+                },
+                latitude: 33.524940,
+                longitude: -111.951362
+            },
+            {
+                id: 13,
+                options: {
+                    title: 'jamest',
+                },
+                latitude: 33.590820,
+                longitude: -111.748194
+            },
+            {
+                id: 14,
+                options: {
+                    title: 'djkahled',
+                },
+                latitude: 33.582163,
+                longitude: -111.763023
+            },
+            {
+                id: 15,
+                options: {
+                    title: 'deadmau5',
+                },
+                latitude: 33.576294,
+                longitude: -111.734037
+            },
+            {
+                id: 16,
+                options: {
+                    title: 'stephenisme',
+                },
+                latitude: 33.557789,
+                longitude: -111.889232
+            },
+            {
+                id: 17,
+                options: {
+                    title: 'mikeisme',
+                },
+                latitude: 33.469585,
+                longitude: -111.748802
+            },
+            {
+                id: 18,
+                options: {
+                    title: 'bigdaddy',
+                },
+                latitude: 33.540272,
+                longitude: -111.868736
+            },
         ];
 
     }
